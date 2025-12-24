5b5dfaa830d1db64f089b36e13a7b579e019895b
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2025-12-25 00:03:50 +0200

    add CORS support

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 
     # my apps
     'bookings',
@@ -136,5 +137,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000", #react dev server
 ]
