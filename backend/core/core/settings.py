d42ee46064c6d15d8502042f95c1a88e4c5ffd7f
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2025-12-24 20:28:10 +0200

    initialize bookings, users, and workspaces apps with basic structure

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'booking',
+    'users',
+    'workspaces',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
