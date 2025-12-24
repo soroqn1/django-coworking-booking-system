f4f94adbb95385deb6aa8739ba2b0019cdca6d52
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2025-12-25 01:00:59 +0200

    add CORS configuration

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -140,3 +141,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000", #react dev server
 ]
+
+CORS_ALLOW_CREDENTIALS = True
